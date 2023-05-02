e03b317af9bdf97840e75c40fa69fd75a6230f46
ShreeSinghi <singhishree@gmail.com>
2023-05-02 20:46:32 +0530

    WIP

--- script.js
+++ script.js
@@ -17,11 +17,17 @@ sprite2.src = './player2.png'
 // y_max is 0.5625
 
 function gaussian(stdev=1) {
-    let u = 1 - Math.random()
-    let v = Math.random()
-    let z = Math.sqrt( -2.0 * Math.log( u ) ) * Math.cos( 2.0 * Math.PI * v )
+    var u = 1 - Math.random()
+    var v = Math.random()
+    var z = Math.sqrt( -2.0 * Math.log( u ) ) * Math.cos( 2.0 * Math.PI * v )
     return z * stdev;
 }
+
+function rotate(vx, vy, theta) {
+    return {x : vx * Math.cos(theta) - vy * Math.sin(theta),
+            y : vx * Math.sin(theta) + vy * Math.cos(theta)}
+}
+
 
 window.addEventListener("keydown",
     function(e){
@@ -42,11 +48,8 @@ function inter_c_c(circ1, circ2) {
     var x = circ1.x-circ2.x;
     var y = circ1.y-circ2.y;
     var dist = (x**2 + y**2)**0.5
-    if ( dist <= circ1.radius+circ2.radius){
-        return (true, x/dist, y/dist)
-    } else {
-        return (false, 0, 0)
-    }
+    return [dist <= circ1.radius+circ2.radius, x/dist, y/dist]
+
 }
 
 function inter_c_r(circ, rect) {
@@ -54,11 +57,11 @@ function inter_c_r(circ, rect) {
     // direction represents relative direction circle should "bounce" towards to negate collision
     // direction functionality is not working yet
     
-    let x = (circ.x - rect.x);
-    let y = (circ.y - rect.y);
-    let colliding = false;
-    let direction_x = 0;
-    let direction_y = 0;
+    var x = (circ.x - rect.x);
+    var y = (circ.y - rect.y);
+    var colliding = false;
+    var direction_x = 0;
+    var direction_y = 0;
 
     if (Math.abs(x) > rect.width/2 + circ.radius)  { return [false, 0, 0] }
     if (Math.abs(y) > rect.height/2 + circ.radius) { return [false, 0, 0] }
@@ -90,10 +93,30 @@ function inter_c_r(circ, rect) {
     return [(cornerDist <= circ.radius**2) || colliding, direction_x, direction_y]
 }
 
+function c_c_elastic(circ1, circ2){
+    resx = circ1.xspeed - circ2.xspeed
+    resy = circ1.yspeed - circ2.yspeed
+
+    var m1 = circ1.mass
+    var m2 = circ2.mass
+    var theta = -Math.atan2(circ2.y - circ1.y, circ2.x - circ1.x);
+    var v1 = rotate(circ1.speedx, circ1.speedy, theta);
+    var v2 = rotate(circ2.speedx, circ2.speedy, theta);
+    var u1 = rotate(v1.x * (m1 - m2)/(m1 + m2) + v2.x * 2 * m2/(m1 + m2), v1.y, -theta);
+    var u2 = rotate(v2.x * (m2 - m1)/(m1 + m2) + v1.x * 2 * m1/(m1 + m2), v2.y, -theta);
+    
+    circ1.speedx = u1.x
+    circ1.speedy = u1.y
+    circ2.speedy = u2.x
+    circ2.speedy = u2.y
+}
 
 function startGame() {
-    player1 = new Player(0.3, 0.3, sprite1, 'a', 'd', 'w', 's');
-    player2 = new Player(0.7, 0.1, sprite2, 'ArrowLeft', 'ArrowRight', 'ArrowUp', 'ArrowDown');
+    myGameArea.start()
+    ctx = myGameArea.context
+
+    player1 = new Player(0.3, 0.3, sprite1, 'a', 'd', 'w', 's', 0.2);
+    player2 = new Player(0.7, 0.1, sprite2, 'ArrowLeft', 'ArrowRight', 'ArrowUp', 'ArrowDown', 0.8);
 
     floors.push(new Floor(0.1, 0.4, 0.9, 0.44))
     floors.push(new Floor(0.7, 0.3, 0.9, 0.34))
@@ -103,9 +126,6 @@ function startGame() {
     floors.push(new Floor(-0.1      , 0.5625 - border, 1.1       , 0.6625    ))
     floors.push(new Floor(-0.1      , -0.1           , 0 + border, 0.6625    ))
     floors.push(new Floor(1 - border, -0.1           , 1.1       , 0.6625    ))
-
-    myGameArea.start();
-    ctx = myGameArea.context
 
     floorImg = new Image()
     floorImg.src = './brick.png'
@@ -152,7 +172,7 @@ function Floor(x1, y1, x2, y2, colour){
     }
 }
 
-function Player(x, y, sprite, leftKey, rightKey, upKey, downKey) {
+function Player(x, y, sprite, leftKey, rightKey, upKey, downKey, healthX) {
     this.ammo = 0
     this.x = x
     this.y = y
@@ -160,18 +180,36 @@ function Player(x, y, sprite, leftKey, rightKey, upKey, downKey) {
     this.sprite = sprite
     this.speedx = 0
     this.speedy = 0
+    this.health = 1
+    this.mass = 1
 
     this.leftKey  = leftKey
     this.rightKey = rightKey
     this.upKey    = upKey
     this.downKey  = downKey
 
+    this.healthX = healthX
+    this.healthY = 0.05
+    this.healthWidth  = 0.2
+    this.healthHeight = 0.02
+
+    gradient = ctx.createLinearGradient((this.healthX-this.healthWidth/2)*canvas_width,
+                                        (this.healthY-this.healthHeight*3)*canvas_width,
+                                        (this.healthX+this.healthWidth/2)*canvas_width,
+                                        (this.healthY+this.healthHeight*3)*canvas_width)
+
+    gradient.addColorStop(0  , "#ff0000")
+    gradient.addColorStop(0.5, "#f9ff00")
+    gradient.addColorStop(1  , "#12ff00")
+
+    this.gradient = gradient
+
     this.render = function() {
         ctx.save()
 
         ctx.translate(this.x*canvas_width, this.y*canvas_width)
 
-        let theta = this.x/this.radius
+        var theta = this.x/this.radius
         ctx.rotate(theta);
 
         ctx.drawImage(
@@ -182,6 +220,20 @@ function Player(x, y, sprite, leftKey, rightKey, upKey, downKey) {
             this.radius*canvas_width*2
         )
         ctx.restore()
+
+        ctx.strokeStyle = "rgba(0, 0, 0, 1)"
+        ctx.fillStyle = this.gradient
+        ctx.beginPath()
+
+        ctx.roundRect((this.healthX-this.healthWidth/2)*canvas_width,
+                      (this.healthY-this.healthHeight/2)*canvas_width,
+                      this.healthWidth*canvas_width*this.health,
+                      this.healthHeight*canvas_width,
+                      this.healthHeight*canvas_width/2)
+                     
+        ctx.stroke()
+        ctx.fill()
+
     }
     this.move = function() {
         maxSpeed = 0.01
@@ -242,12 +294,27 @@ function updateGameArea() {
         quakeX += (gaussian(0.2)) * (2**Math.abs(player.speedx) + 2**Math.abs(player.speedy) - 2)
         quakeY += (gaussian(0.2)) * (2**Math.abs(player.speedx) + 2**Math.abs(player.speedy) - 2)
     })
+    
+    // ERROR IS HERE!!
+    
+    var temp = inter_c_c(player1,player2)
+    colliding  = temp[0]
+    direction_x = temp[1]
+    direction_y = temp[2]
+    // console.log(colliding)
 
-    if (inter_c_c(player1, player2)){
+
+    if (colliding){
+        c_c_elastic(player1, player2)
+
         var relSpeed = (player1.speedx - player2.speedx)**2 + (player1.speedy - player2.speedy)**2
-        if (relSpeed>0.01){
+        if (relSpeed>0.0001){
             speed1 = player1.speedx**2 + player1.speedy**2
             speed2 = player2.speedx**2 + player2.speedy**2
+
+            player1.health -= 2000000 * speed1 * relSpeed
+            player2.health -= 2000000 * speed2 * relSpeed
+
         }
     }
     
@@ -255,7 +322,6 @@ function updateGameArea() {
 
     ctx.save()
     ctx.translate(canvas_width*quakeX, canvas_width*quakeY)
-    console.log(quakeX, quakeY)
     player1.render()
     player2.render()
 
